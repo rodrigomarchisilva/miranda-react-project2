9d25f2abb90720b1c5904250c8a4c4f2b53a7ac2
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-01-26 15:16:25 -0300

    :sparkles: feat(new): add a counter with logic to increase and decrease it, and a way to reverse the spinning direction of the logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const handleCounter = (action) => {
+    setCounter(counter + (action === '+' ? 1 : -1));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className={reverse ? 'App-logo-reverse' : 'App-logo'}
+          alt="logo"
+        />
+        <button onClick={handleClick}>Reverse</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Counter is at <code>{counter}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => handleCounter('+')}>+</button>
+        <button onClick={() => handleCounter('-')}>-</button>
       </header>
     </div>
   );
 }
-
-export default App;
