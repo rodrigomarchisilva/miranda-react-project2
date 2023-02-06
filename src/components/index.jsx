302d29fb3e1d4eafbc52a015a060fde12b674d32
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-02-06 11:47:04 -0300

    :sparkles: feat(new): create an index file for contexts and components to make importations easier

---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+import { ColoredDiv } from './ColoredDiv';
+import { CompoundComponent } from './CompoundComponent';
+import { CompoundContext } from './CompoundContext';
+import { Counter } from './Counter';
+import { ErrorBoundary } from './ErrorBoundary';
+import { ErrorComponent } from './ErrorComponent';
+import { Loading } from './Loading';
+import { Posts } from './Posts';
+import { LazyParent } from './LazyParent';
+
+export {
+  ColoredDiv,
+  CompoundComponent,
+  CompoundContext,
+  Counter,
+  ErrorBoundary,
+  ErrorComponent,
+  Loading,
+  Posts,
+  LazyParent,
+};
---
+++ src/contexts/index.jsx
@@ -0,0 +1,15 @@
+import { CounterProvider } from './CounterProvider';
+import { CounterContext } from './CounterProvider/context';
+import { PostsProvider } from './PostsProvider';
+import { PostsContext } from './PostsProvider/context';
+import { TurnOnOffProvider } from './TurnOnOffProvider';
+import { TurnOnOffContext } from './TurnOnOffProvider/context';
+
+export {
+  CounterProvider,
+  CounterContext,
+  PostsProvider,
+  PostsContext,
+  TurnOnOffProvider,
+  TurnOnOffContext,
+};
