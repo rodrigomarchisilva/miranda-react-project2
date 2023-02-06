16655e8afeb4565ab418670b77296994defb9558
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-02-06 11:46:24 -0300

    :sparkles: feat(new): create a LazyComponent example

---
+++ src/components/LazyComponent/index.jsx
@@ -0,0 +1,3 @@
+const LazyComponent = () => <h1>Lazy Component</h1>;
+
+export default LazyComponent;
---
+++ src/components/LazyParent/index.jsx
@@ -0,0 +1,15 @@
+import { useState, Suspense, lazy } from 'react';
+import { Loading } from '../Loading';
+
+const LazyComponent = lazy(() => import('../LazyComponent'));
+
+export const LazyParent = () => {
+  const [show, setShow] = useState(false);
+  const toggle = () => setShow(!show);
+  return (
+    <div>
+      <button onClick={toggle}>Show/Hide Lazy Component</button>
+      <Suspense fallback={<Loading />}>{show && <LazyComponent />}</Suspense>
+    </div>
+  );
+};
