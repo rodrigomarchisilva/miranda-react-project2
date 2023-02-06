598ee391156d4537a48f5123394635d1bfc0a5f2
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-02-06 11:40:04 -0300

    :bulb: chore(comments): comment tests to work on them later

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders counter', () => {
-  render(<App />);
-  const counter = screen.getByText(/Counter is at/i);
-  expect(counter).toBeInTheDocument();
-});
+// test('renders counter', () => {
+//   render(<App />);
+//   const counter = screen.getByText(/Counter is at/i);
+//   expect(counter).toBeInTheDocument();
+// });
