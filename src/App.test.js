1af2c91829eee06030bea6ebb75a533090e0dce2
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2023-02-07 21:13:48 -0300

    :fire: refactor(delete file): remove unnecessary files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders counter', () => {
-//   render(<App />);
-//   const counter = screen.getByText(/Counter is at/i);
-//   expect(counter).toBeInTheDocument();
-// });
